6d3077192ec6efb5885425c8382730dbe4dc6136
valentinmadiot <valentin.madiot@gmail.com>
2025-06-04 14:39:26 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => (
+  <section className="home">
+    <h1>Composants d'Authentification</h1>
+    <p>Découvrez différentes interfaces d'inscription et de connexion.</p>
+  </section>
+);
+
+export default Home;
