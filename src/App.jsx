b319e3413cd86108beab77b6f338532573f66c73
valentinmadiot <valentin.madiot@gmail.com>
2025-06-06 00:57:32 +0700

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-// import AuthForm1 from "./components/auth-form-1/AuthForm1";
-// import AuthForm2 from "./components/auth-form-2/AuthForm2";
-// import AuthForm3 from "./components/auth-form-3/AuthForm3";
-// import AuthForm4 from "./components/auth-form-4/AuthForm4";
+import AuthForm1 from "./components/auth-form-1/AuthForm1";
+import AuthForm2 from "./components/auth-form-2/AuthForm2";
+import AuthForm3 from "./components/auth-form-3/AuthForm3";
+import AuthForm4 from "./components/auth-form-4/AuthForm4";
 import AuthForm5 from "./components/auth-form-5/AuthForm5";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-      {/* <Home /> */}
-      {/* <AuthForm1 /> */}
-      {/* <AuthForm2 /> */}
-      {/* <AuthForm3 /> */}
-      {/* <AuthForm4 /> */}
+      <Home />
+      <AuthForm1 />
+      <AuthForm2 />
+      <AuthForm3 />
+      <AuthForm4 />
       <AuthForm5 />
     </div>
   );
